cf3c19a56eac4e986e18dd573d26055973cfd1cf
Stefan Ivanovski <sivanovski.dev@gmail.com>
2023-02-27 18:38:00 +0100

    Add JournalViewModel factory

---
+++ app/src/main/java/mk/ukim/finki/coffeejournal/viewmodels/JournalViewModelFactory.kt
@@ -0,0 +1,18 @@
+package mk.ukim.finki.coffeejournal.viewmodels
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import mk.ukim.finki.coffeejournal.repository.JournalEntryRepository
+import mk.ukim.finki.coffeejournal.room.RoomDataSource
+import mk.ukim.finki.coffeejournal.room.database.AppDatabase
+
+class JournalViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(JournalEntryRepository::class.java).newInstance(
+            JournalEntryRepository(
+                RoomDataSource(AppDatabase.getDatabase(context).journalEntryDao())
+            )
+        )
+    }
+}
