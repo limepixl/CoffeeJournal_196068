bd6745c793b8614ab99a0b6ffc125260fe804f2d
Stefan Ivanovski <sivanovski.dev@gmail.com>
2023-02-27 19:24:54 +0100

    Moved from storing separate members to storing a single JournalEntry member

--- app/src/main/java/mk/ukim/finki/coffeejournal/viewmodels/JournalViewModel.kt
+++ app/src/main/java/mk/ukim/finki/coffeejournal/viewmodels/JournalViewModel.kt
@@ -1,45 +1,59 @@
 package mk.ukim.finki.coffeejournal.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import mk.ukim.finki.coffeejournal.enums.BrewMethod
-import java.util.Date
+import mk.ukim.finki.coffeejournal.repository.JournalEntryRepository
+import mk.ukim.finki.coffeejournal.room.model.JournalEntry
+import java.util.*
 
 @Suppress("unused")
-class JournalViewModel : ViewModel() {
-    private var _date: Date = Date()
-    private var _brewMethod: BrewMethod = BrewMethod.ESPRESSO
-    private var _rating: Int = 0
-    private var _notes: String = ""
+class JournalViewModel(val journalEntryRepository: JournalEntryRepository) : ViewModel() {
+    private val journalEntriesLiveData = MutableLiveData<List<JournalEntry>>()
+    private val journalEntry: JournalEntry = JournalEntry()
+
+    fun getJournalEntriesLiveData(): LiveData<List<JournalEntry>> = journalEntriesLiveData
+
+    fun listAll() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val journalEntries = journalEntryRepository.getAll()
+            journalEntriesLiveData.postValue(journalEntries)
+        }
+    }
 
     fun setDate(date: Date) {
-        _date = date
+        journalEntry.date = date.time
     }
 
     fun getDate() : Date {
-        return _date
+        return Date(journalEntry.date)
     }
 
     fun setBrewMethod(brewMethod: BrewMethod) {
-        _brewMethod = brewMethod
+        journalEntry.brewMethod = brewMethod
     }
 
     fun getBrewMethod() : BrewMethod {
-        return _brewMethod
+        return journalEntry.brewMethod
     }
 
     fun setRating(rating: Int) {
-        _rating = rating.coerceIn(0, 5)
+        journalEntry.rating = rating.coerceIn(0, 5)
     }
 
     fun getRating() : Int {
-        return _rating
+        return journalEntry.rating
     }
 
     fun setNotes(notes: String) {
-        _notes = notes
+        journalEntry.notes = notes
     }
 
-    fun getNotes() : String {
-        return _notes
+    fun getNotes() : String? {
+        return journalEntry.notes
     }
 }
