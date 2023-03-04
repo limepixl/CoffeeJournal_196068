85bb7f30ae30e018b82ace06b8ef11a2e08b718b
Stefan Ivanovski <sivanovski.dev@gmail.com>
2023-03-04 12:14:51 +0100

    Restructure folder hierarchy

--- app/src/main/java/mk/ukim/finki/coffeejournal/fragments/ViewJournalEntriesFragment.kt
+++ app/src/main/java/mk/ukim/finki/coffeejournal/fragments/ViewJournalEntriesFragment.kt
@@ -1,17 +1,18 @@
-package mk.ukim.finki.coffeejournal
+package mk.ukim.finki.coffeejournal.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import mk.ukim.finki.coffeejournal.R
 
-class HandbookFragment : Fragment() {
+class ViewJournalEntriesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false)
+        return inflater.inflate(R.layout.fragment_view_journal_entries, container, false)
     }
 }
