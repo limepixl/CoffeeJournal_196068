a515b88186f54ad5e2b40990aa8a653076177eb9
Stefan Ivanovski <sivanovski.dev@gmail.com>
2023-03-18 14:18:05 +0100

    Added Google Cloud Messaging plugin and notifications permission

--- app/src/main/java/mk/ukim/finki/coffeejournal/MainActivity.kt
+++ app/src/main/java/mk/ukim/finki/coffeejournal/MainActivity.kt
@@ -1,7 +1,12 @@
 package mk.ukim.finki.coffeejournal
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,10 +17,18 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var fragmentContainerView: FragmentContainerView
+    private var askedForPermission = false
+
+    override fun onResume() {
+        super.onResume()
+        askNotificationPermission()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        askNotificationPermission()
 
         bottomNavigationView = findViewById(R.id.bottom_nav_view)
         fragmentContainerView = findViewById(R.id.fragmentContainerView)
@@ -34,5 +47,39 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
+    }
+
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (!askedForPermission) {
+            if (isGranted) {
+                // FCM SDK (and your app) can post notifications.
+            } else {
+                android.app.AlertDialog.Builder(this).setTitle("Notifications turned off!")
+                    .setMessage("We send a daily notification reminding you to log your coffee. If you would like to use that feature, go into the app's settings and allow the notification permission. Thank you!")
+                    .setPositiveButton("Okay", null).show()
+            }
+
+            askedForPermission = true
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
 }
